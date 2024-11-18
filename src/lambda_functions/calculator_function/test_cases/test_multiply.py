ca4c3ba0b0801b6133ef7d595f97a0e20bd6a4b7
gauravpatel67 <gdp110501@gmail.com>
2024-11-18 12:27:57 +0530

    correct

--- src/lambda_functions/calculator_function/test_cases/test_multiply.py
+++ src/lambda_functions/calculator_function/test_cases/test_multiply.py
@@ -11,4 +11,4 @@ def test_multiply():
     result = lambda_handler(event, None)
 
     assert result["statusCode"] == 200  # Check for success status code
-    assert result["body"]["result"] == 1  # Check for correct multiplication result
+    assert result["body"]["result"] == 13  # Check for correct multiplication result
